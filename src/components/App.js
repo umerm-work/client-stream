51f0dc72cd17073162c47b3074cd02e74d3864c4
Umer <umerm.work@gmail.com>
2019-03-26 01:10:11 +0500

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,23 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from './Header';
 
 const App = () => {
     return (
-    <div>
-        <BrowserRouter>
-        <div>
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" component={StreamCreate} />
-            <Route path="/streams/edit" component={StreamEdit} />
-            <Route path="/streams/delete" component={StreamDelete} />
-            <Route path="/streams/show" component={StreamShow} />
-        </div>
-        </BrowserRouter>    
-    </div>)
-    ;
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
+                </div>
+            </BrowserRouter>
+        </div>)
+        ;
 };
 
 export default App;
