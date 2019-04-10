9e5679581538d18bc4e38dba0754ee4862fd77a0
Umer <umerm.work@gmail.com>
2019-04-10 23:08:03 +0500

    Added stream reducer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -8,6 +8,14 @@ import {
 
 export default ( state = {}, action) => {
     switch(action.type){
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]:action.payload}
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]:action.payload}
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]:action.payload}
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
