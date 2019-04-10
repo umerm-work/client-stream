116f2fa88b010131255e841fa945dd6ff777a52b
Umer <umerm.work@gmail.com>
2019-04-10 23:18:02 +0500

    Added to cobined reduxer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -8,6 +8,8 @@ import {
 
 export default ( state = {}, action) => {
     switch(action.type){
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload,'id')}
         case FETCH_STREAM:
             return { ...state, [action.payload.id]:action.payload}
         case CREATE_STREAM:
