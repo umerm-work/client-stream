bda0cdb38ba425b833519545a228cd3146434b26
Umer <umerm.work@gmail.com>
2019-04-12 00:57:45 +0500

    Add history

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,7 +27,10 @@ export const createStream = formValues => async (dispatch, getState) => {
     const { userId } = getState().auth
     const response = await streams.post('/streams',{...formValues, userId});
 
+
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    // Do some programmatic navigation to
+    // get the user back to the route
 };
 
 export const fetchStreams = () => async dispatch => {
