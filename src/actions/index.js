8568c5cd4452b503525529963f93508d4eadc085
Umer <umerm.work@gmail.com>
2019-04-22 00:56:03 +0500

    Fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -56,5 +56,7 @@ export const deleteStream = (id) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id})
+
+    history.push('/')
 };
 
